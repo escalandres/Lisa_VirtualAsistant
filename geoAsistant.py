bae9cb967ea49a5634824d5da35d581e83b3db7f
Andres Escala <79557629+escalandres@users.noreply.github.com>
2023-05-31 23:26:23 -0600

    nuevo asistente

--- geoAsistant.py
+++ geoAsistant.py
@@ -16,19 +16,6 @@ import datetime
 #import security_camera
 import requests
 
-def convertir_libras_a_pesos(libras):
-    url = "https://api.exchangerate-api.com/v4/latest/GBP"  # API para obtener la tasa de cambio
-    response = requests.get(url)
-    data = response.json()
-    tasa_cambio = data["rates"]["MXN"]  # Tasa de cambio de libras a pesos mexicanos
-    pesos = libras * tasa_cambio
-    return pesos
-
-contactos = {
-    'andrew':'+525545464585',
-    'mom':'+525545460034',
-    'jorge':'+56932739269'
-}
 #-----------------------------------------------------------------------------------------
 #Declaracion de funciones
 def talk(text):
@@ -111,49 +98,10 @@ def run_alexa():
         info = wikipedia.summary(person, 1)
         print(info)
         talk('wikipedia result:'+info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'are you there' in command:
-        talk('Yes, I am here')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    elif 'alpha' in command:
-        talk('Opening WolframAlpha')
-        Wolfram()
     elif 'goodbye' in command:
         global close
         close=1
         talk('Okay')
-    elif 'convert money' in command:
-        # Ejemplo de uso
-        talk("Enter the amount of British Pounds to convert into Mexican pesos")
-        libras = float(input("British Pounds: "))
-        pesos_mexicanos = convertir_libras_a_pesos(libras)
-        print(f"{libras} British Pounds equals to {pesos_mexicanos} Mexican pesos.")
-        talk(f"{libras} British Pounds equals to {pesos_mexicanos} Mexican pesos.")
-    elif 'what is my internet speed' in command:
-        st = speedtest.Speedtest()
-        talk('I am checking your internet speed. Please give a minute')
-        #velocidad de descarga
-        d=st.download()
-        d=(d*pow(10,-6)) #se hace la conversion de bits a megabits
-        strdownload = format(d,".2f") #se convierte a dos decimales
-        u=st.upload()
-        u=(u*pow(10,-6))
-        strupload = format(u,".2f")
-        p=st.results.ping
-        strping = str(p)
-        #download
-        print('Your download speed is: '+strdownload+' MB/s')
-        talk('Your download speed is: '+strdownload+' Megabytes per second')
-        #Velocidad de carga
-        print('Your upload speed is: '+strupload+' MB/s')
-        talk('Your upload speed is: '+strupload+' Megabytes per second')
-        #El ping
-        print('Your internet ping is: '+strping+' ms')
-        talk('Yor internet ping is: '+strping+' miliseconds')
     elif 'search for' in command:
         search = command.replace('search for', '')
         pywhatkit.search(search)
@@ -173,37 +121,6 @@ def run_alexa():
                 tts.save(f''+path_to_save+'\\'+"voiceNote_{x}.txt")
         except:
             pass
-    elif 'send a message' in command:
-        er=False
-        talk('Ok, To whom do you want to send this message?')
-        while er == False:
-            try:
-                name=take_command()
-                if 'to' in name:
-                    name=name.replace('to','')
-                elif 'two' in name:
-                    name=name.replace('two','')
-                receiver=contactos[name]
-                er=True
-            except:
-                print("I didn't understand you. Please repeat it again")
-                talk("I didn't understand you. Please repeat it again")
-        talk('Ok, what do you want to send to '+name+'?')
-        message=get_info()
-        h = datetime.datetime.now().strftime('%H')
-        hour=int(h)
-        mi = datetime.datetime.now().strftime('%M')
-        minute=int(mi)+1
-        print('Message to'+name+' = '+receiver+' at '+h+':'+mi)
-        print('OK, your message will be sent in one minute. Please wait!')
-        talk('OK, your message will be sent in one minute. Please wait!')
-        pywhatkit.sendwhatmsg(receiver,message,hour,minute)
-        #pywhatkit.sendwhatmsg(contactos['Tere'],"This is a message",17,10)
-        talk('Ok, your message has been sent to '+name)
-    elif 'show me the camera' in command:
-        print('Opening camera...')
-        talk('Ok, opening camera')
-        os.system('python security_camera.py')
     else:
         talk('Please say the command again.')
 
